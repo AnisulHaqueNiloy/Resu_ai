0b1f7f22204a879e49feefe122325737822edd48
niloy <niloy2931@gmail.com>
2025-04-28 12:52:47 +0600

    redirect added

--- apiendpoint/GlobalApi.js
+++ apiendpoint/GlobalApi.js
@@ -1,8 +1,11 @@
 import axios from "axios";
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 const API_KEY = import.meta.env.VITE_STRAPI_API_KEY;
 const axiosClient = axios.create({
-  baseURL: "http://localhost:1337/api/",
+  baseURL: import.meta.env.VITE_BASE_URL + "/api/",
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${API_KEY}`,
